560fb6de05e395921d034bbbfaffb0c99b021e88
Cacing <ibnuul@gmail.com>
2017-06-20 15:33:25 +0700

    [*]Redesign

--- src/form/home.java
+++ src/form/home.java
@@ -40,7 +40,7 @@ public class home extends javax.swing.JFrame {
             var.mkdir();
             tmp.mkdir();
         }
-        this.setLocationRelativeTo(this);
+        //this.setLocationRelativeTo(this);
         tampilTabel();
     }
     @SuppressWarnings("unchecked")
@@ -76,11 +76,12 @@ public class home extends javax.swing.JFrame {
                 formWindowOpened(evt);
             }
         });
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().setLayout(null);
 
         jLabel5.setForeground(new java.awt.Color(51, 51, 51));
         jLabel5.setText("COEM | 2017");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(433, 242, 70, -1));
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(433, 242, 70, 14);
 
         btnClose.setBackground(new java.awt.Color(34, 168, 109));
         btnClose.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -99,7 +100,8 @@ public class home extends javax.swing.JFrame {
                 btnCloseMouseExited(evt);
             }
         });
-        getContentPane().add(btnClose, new org.netbeans.lib.awtextra.AbsoluteConstraints(393, 120, 85, 32));
+        getContentPane().add(btnClose);
+        btnClose.setBounds(393, 120, 85, 32);
 
         btnOpen.setBackground(new java.awt.Color(34, 168, 109));
         btnOpen.setForeground(new java.awt.Color(51, 51, 51));
@@ -119,30 +121,33 @@ public class home extends javax.swing.JFrame {
                 btnOpenMouseExited(evt);
             }
         });
-        getContentPane().add(btnOpen, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 85, 32));
+        getContentPane().add(btnOpen);
+        btnOpen.setBounds(30, 120, 85, 32);
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/text_1.png"))); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(172, 80, 170, -1));
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(172, 80, 170, 23);
 
         jLabel2.setForeground(new java.awt.Color(51, 51, 51));
         jLabel2.setText("Copyright© Cacing");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 242, 100, -1));
-        jLabel2.getAccessibleContext().setAccessibleName("Copyright© Cacing");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(14, 242, 100, 14);
 
         txt_File.setEditable(false);
         txt_File.setBackground(new java.awt.Color(255, 255, 255));
         txt_File.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 18)); // NOI18N
         txt_File.setForeground(new java.awt.Color(51, 51, 51));
-        txt_File.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txt_File.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 51)));
         txt_File.setCaretColor(new java.awt.Color(51, 51, 51));
         txt_File.setDisabledTextColor(new java.awt.Color(51, 51, 51));
         txt_File.setEnabled(false);
-        getContentPane().add(txt_File, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 165, 450, 30));
+        getContentPane().add(txt_File);
+        txt_File.setBounds(30, 165, 450, 30);
 
         jLabel1.setFont(new java.awt.Font("Ubuntu", 2, 10)); // NOI18N
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/logo.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(173, 20, 160, 70));
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(173, 20, 160, 70);
 
         btnSave.setBackground(new java.awt.Color(34, 168, 109));
         btnSave.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -161,7 +166,8 @@ public class home extends javax.swing.JFrame {
                 btnSaveMouseExited(evt);
             }
         });
-        getContentPane().add(btnSave, new org.netbeans.lib.awtextra.AbsoluteConstraints(175, 210, 160, 32));
+        getContentPane().add(btnSave);
+        btnSave.setBounds(175, 210, 160, 32);
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/bg.jpg"))); // NOI18N
         jLabel3.setText("jLabel3");
@@ -175,7 +181,8 @@ public class home extends javax.swing.JFrame {
                 jLabel3MousePressed(evt);
             }
         });
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 490, 250));
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(10, 10, 490, 250);
 
         jPanel1.setBackground(new java.awt.Color(47, 47, 47));
         jPanel1.setForeground(new java.awt.Color(47, 47, 47));
@@ -201,7 +208,8 @@ public class home extends javax.swing.JFrame {
             .addGap(0, 270, Short.MAX_VALUE)
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 510, 270);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -228,7 +236,7 @@ public class home extends javax.swing.JFrame {
                     try {
                         xc_xmlRemove.xmlRemovePassword(xc_sheetList.getSheetName().values().toArray()[i]+".xml", "sheetProtection");
                     } catch (Exception e) {
-                        System.out.println(e.getLocalizedMessage().toString());
+                        System.out.println(e.getLocalizedMessage());
                     }
                 }
                 try {
@@ -340,7 +348,7 @@ public class home extends javax.swing.JFrame {
             if (xc_extension.getFileExtension(uxcel.getFileLocation()).equals("xlsx")) { //office 2010
                 crack2010();
             } else if(xc_extension.getFileExtension(uxcel.getFileLocation()).equals("xls")){
-                System.out.println("Running <= 2007");
+                JOptionPane.showMessageDialog(this, "File not supported, if your file is XLS, u can open it first, then save as XLSX type!");
             }
         }
             else {
@@ -402,6 +410,7 @@ public class home extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
+    @SuppressWarnings("Convert2Lambda")
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -428,6 +437,7 @@ public class home extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 home gui = new home();
 //                gui.setUndecorated(true);
